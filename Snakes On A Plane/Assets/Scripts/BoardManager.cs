fe1c9fa79817227e415e494054c53ba8f416d488
tesu <jasonlam@mit.edu>
2018-03-10 19:43:09 -0500

    got movement to only register on the beats (no visual rep)

--- Snakes On A Plane/Assets/Scripts/BoardManager.cs
+++ Snakes On A Plane/Assets/Scripts/BoardManager.cs
@@ -9,11 +9,15 @@ public class BoardManager : MonoBehaviour {
 	public GameObject player_prefab; // for different players just have two different prefabs here
 	public Color[] player_colors = {Color.red, Color.blue}; // remove these simple colorings when we have real characters/animations
 	public Color dead_tile_color = Color.black;
+    public float initial_offset;
+    public float seconds_per_beat;
+    public float delta_time;
 
 	private GameObject[,] board; // just x, y coordinates
 	private GameObject[] players; // if we end up with a lot of player-related variables, just make a Player class
 	private Vector2[] player_positions;
 	private Dictionary<string, Vector2> directions;
+    private float time_offset;
 
 	// Use this for initialization
 	void Start () {
@@ -37,10 +41,15 @@ public class BoardManager : MonoBehaviour {
 		directions.Add ("Right", new Vector2 (1, 0));
 		directions.Add ("Up", new Vector2 (0, 1));
 		directions.Add ("Down", new Vector2 (0, -1));
+
+        GetComponent<AudioSource>().Play();
+        time_offset = initial_offset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        time_offset += Time.deltaTime;
+        while (time_offset >= seconds_per_beat) time_offset -= seconds_per_beat;
 		// take player inputs
 		for (int i = 0; i < 2; i++) {
 			foreach (string key in directions.Keys) {
@@ -52,6 +61,12 @@ public class BoardManager : MonoBehaviour {
 	}
 
 	void TryMovePlayer(int p_num, string key) {
+        if (time_offset > delta_time && time_offset < seconds_per_beat - delta_time)
+        {
+            Debug.Log(time_offset);
+            return;
+        }
+
 		Vector2 new_position = player_positions [p_num] + directions [key];
 		if (IsPositionAllowed (new_position)) {
 			board [(int)player_positions [p_num].x, (int)player_positions [p_num].y].GetComponent<Renderer> ().material.color = dead_tile_color;
