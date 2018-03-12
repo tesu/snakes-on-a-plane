8eeb2ba37d1d8feb30e2c9252803a4c91366ea83
Angel Alvarez <alvareza@mit.edu>
2018-03-12 13:05:32 -0400

    Added prefabs and scripts for beats and notes

---
+++ Snakes On A Plane/Assets/Scripts/beatVisualizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beatVisualizer : MonoBehaviour {
+
+	public GameObject note_prefab;
+	public float note_spawn_shift_left;
+	public float note_spawn_shift_up;
+	public Vector2 note_spawn_shift;
+	// Use this for initialization
+	void Start () {
+		note_spawn_shift = new Vector2(note_spawn_shift_left, note_spawn_shift_up);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  GameObject note = Instantiate(note_prefab);
+		note.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -2);
+		note.transform.position = (Vector2) this.transform.position + note_spawn_shift + new Vector2(Random.Range(-0.5f, 0.5f),0);
+	}
+}
---
+++ Snakes On A Plane/Assets/Scripts/note.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class note : MonoBehaviour {
+
+	public int lifetime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime--;
+
+		if (lifetime <= 0) {
+			Destroy(this);
+		}
+	}
+}
