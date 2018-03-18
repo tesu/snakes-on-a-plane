86260e4353e1a50fe6ba35900746f6a3e447f465
gerr <gerr@mit.edu>
2018-03-18 08:06:57 -0400

    SCENE HELL
    -added rules, credits scenes, with proper info on them
    -hella updated the post-game scene!! now is aesthetic af... animated with characters happy or sad depending on win/loss
    -updated a lot of text color things/formatting on scene select
    -new arrow to match white+pixel aesthetic for scene select
    -made sure our pixel assets are not fuzzy (proper 'point' import settings for scaling)
    -made all canvases scale WRT screen size
    -yay animations
    -yay this is like pretty functional at this point :D
    -

--- Snakes On A Plane/Assets/Scripts/EndScreenManager.cs
+++ Snakes On A Plane/Assets/Scripts/EndScreenManager.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class EndScreenManager : MonoBehaviour {
     public GameObject end_text;
     public GameObject red_score;
-    public GameObject blue_score;
+	public GameObject blue_score;
+	public GameObject pinkWin;
+	public GameObject pinkLoss;
+	public GameObject blueWin;
+	public GameObject blueLoss;
 
     // Use this for initialization
     void Start () {
 		if (StaticValues.winner == 1)
         {
-            end_text.GetComponent<UnityEngine.UI.Text>().text = "Red wins!";
-        }
+			end_text.GetComponent<UnityEngine.UI.Text>().text = "Pink wins!";
+			pinkWin.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			pinkLoss.GetComponent<SpriteRenderer> ().sortingOrder = -1;
+			blueWin.GetComponent<SpriteRenderer> ().sortingOrder = -1;
+			blueLoss.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+		}
         else
         {
-            end_text.GetComponent<UnityEngine.UI.Text>().text = "Blue wins!";
+			end_text.GetComponent<UnityEngine.UI.Text>().text = "Blue wins!";
+			pinkWin.GetComponent<SpriteRenderer> ().sortingOrder = -1;
+			pinkLoss.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			blueWin.GetComponent<SpriteRenderer> ().sortingOrder = 1;
+			blueLoss.GetComponent<SpriteRenderer> ().sortingOrder = -1;
+		
         }
-        red_score.GetComponent<UnityEngine.UI.Text>().text = "Red: " + StaticValues.score[0];
+        red_score.GetComponent<UnityEngine.UI.Text>().text = "Pink: " + StaticValues.score[0];
         blue_score.GetComponent<UnityEngine.UI.Text>().text = "Blue: " + StaticValues.score[1];
     }
 	
